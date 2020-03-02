90b376d9317b56ec93f4fe24f115bef919c08701
Kristian Wilson <kristian.wilson@autointegrate.com>
2020-03-02 09:38:53 +0000

    Initial checkin.

---
+++ AutomatedJobApproval.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedJobApproval.Tests
+{
+    public class Class1
+    {
+    }
+}
